df6efb340a333accf8f7c1ae98aa37486bea3c64
ChristopheDevos <christophedevos_666@hotmail.com>
2025-01-14 11:38:22 +0100

    add files for demo

---
+++ src/main/java/com/demo/cybeats/CybeatsApplication.java
@@ -0,0 +1,14 @@
+package com.demo.cybeats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CybeatsApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(CybeatsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/cybeats/CybeatsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.cybeats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CybeatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
